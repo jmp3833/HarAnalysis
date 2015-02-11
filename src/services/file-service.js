31df6454b155f48a73b802b9189ebc5f4ceb4515
Peterson, Justin <Justin_Peterson@intuit.com>
2015-02-11 15:11:30 -0800

    Code cleanup when transitioning to service rather than a command line app

--- src/services/file-service.js
+++ src/services/file-service.js
@@ -1,11 +1,19 @@
-function getReadme(){
-    var filename = __dirname.toString() + '/' +  req.body.file;
-
+/** Test service to integrate with the
+ * angular dashboard application.
+ */
+var fs = require('fs');
+/**
+ * Get readme.md file to send to the frontend application.
+ * @param filename - name of the Readme file
+ */
+function getReadme(filename){
     fs.readFile(filename, 'utf8', function(err, data) {
         if (err) throw err;
         console.log('OK: ' + filename);
-        res.end(data);
+        return data;
     });
 }
 
-exports.getReadme = getReadme();
+module.exports = {
+    getReadme: getReadme
+};
